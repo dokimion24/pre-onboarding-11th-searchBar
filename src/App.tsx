08b63a8ed3c7dc53458fecfc7a92a8b2144b9a29
dokimion24 <leecy3610@gmail.com>
2023-07-16 22:39:42 +0900

    feat: 제목 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import SearchList from './components/SearchList';
 import SearchForm from './components/SearchForm';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 32px;
+  text-align: center;
+`;
 
 function App() {
   return (
     <>
+      <Title>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </Title>
       <SearchForm />
       <SearchList />
     </>

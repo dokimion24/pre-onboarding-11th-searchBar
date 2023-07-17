ec24b83a7a6278bfdb34420f088f57e9c3ba1fa1
dokimion24 <leecy3610@gmail.com>
2023-07-17 18:39:10 +0900

    feat: 스타일 추가

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Sick } from '../apis/search';
+import { styled } from 'styled-components';
 
-const SearchItem = () => {
-  return <div>Item</div>;
+const Item = styled.li`
+  padding: 8px 0;
+`;
+interface Props {
+  sicks: Sick;
+}
+
+const SearchItem = ({ sicks }: Props) => {
+  return <Item>{sicks.sickNm}</Item>;
 };
 
 export default SearchItem;

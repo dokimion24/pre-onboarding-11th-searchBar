6c4f3e64c1c9b0a5b91138ef1993565cb238641f
dokimion24 <leecy3610@gmail.com>
2023-07-19 02:35:53 +0900

    feat: default delay 300으로 변경

--- src/hooks/useDeounce.ts
+++ src/hooks/useDeounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const useDebounce = (value: string, delay: number = 500) => {
+const useDebounce = (value: string, delay: number = 300) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
